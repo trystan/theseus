fa6dabe578b8b7a2b3f9b63ffeb5386700df0589
Trystan <trystan.s@comcast.net>
2024-06-07 16:15:30 -0700

    Navigation facts & graphviz output

--- theseus.ts
+++ theseus.ts
@@ -1,1 +1,31 @@
-console.log('theseus')
+interface NavigationFact {
+  from: string
+  to: string
+}
+
+const facts: NavigationFact[] = [
+  { from: 'a', to: 'b' },
+  { from: 'b', to: 'l1' },
+  { from: 'b', to: 'r1' },
+  { from: 'l1', to: 'c' },
+  { from: 'r1', to: 'c' },
+  { from: 'c', to: 'd' },
+  { from: 'd', to: 'l2' },
+  { from: 'd', to: 'r2' },
+  { from: 'l2', to: 'e' },
+  { from: 'r2', to: 'e' },
+  { from: 'l2', to: 'z1' },
+  { from: 'r2', to: 'z2' },
+  { from: 'e', to: 'z' },
+]
+
+const toGraphvizInput = (facts: NavigationFact[]): string => {
+  const describeEdge = (n: NavigationFact) => {
+    return `  "${n.from}" -> "${n.to}"`
+  }
+  return `digraph { \n${facts.map(describeEdge).join('\n') }\n}`
+}
+
+// -- run -- //
+
+console.log(toGraphvizInput(facts))
